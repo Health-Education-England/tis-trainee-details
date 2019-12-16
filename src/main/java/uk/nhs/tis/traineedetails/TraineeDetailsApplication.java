3f8699d260d1f1654c11622aa771cccc1bc4d5ec
jayanta2018 <jayanta.saha@hee.nhs.uk>
2019-12-16 13:15:06 +0000

    Initial commit with gitignore file

---
+++ src/main/java/uk/nhs/tis/traineedetails/TraineeDetailsApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.tis.traineedetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeDetailsApplication.class, args);
+	}
+
+}
