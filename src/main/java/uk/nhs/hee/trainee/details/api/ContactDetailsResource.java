933aa071603cd8380af1467d45aa34541119ee8f
Babul Reddy Yasa <babul3012@gmail.com>
2019-12-23 16:01:08 +0000

    Temp fix for localhost cross origin sharing

--- src/main/java/uk/nhs/hee/trainee/details/api/ContactDetailsResource.java
+++ src/main/java/uk/nhs/hee/trainee/details/api/ContactDetailsResource.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import uk.nhs.hee.trainee.details.service.ContactDetailsService;
 import uk.nhs.hee.trainee.details.model.ContactDetails;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/trainee")
 public class ContactDetailsResource {
 
