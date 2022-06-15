280378b38bd791d9655b40b05ca808892df642dc
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2022-06-15 15:42:53 +0100

    feat: add TraineeProfile versioning
    This will support optimistic locking.
    
    TIS21-3114: Ensure trainee profiles are always fully populated

--- src/main/java/uk/nhs/hee/trainee/details/TisTraineeDetailsApplication.java
+++ src/main/java/uk/nhs/hee/trainee/details/TisTraineeDetailsApplication.java
@@ -21,9 +21,11 @@
 
 package uk.nhs.hee.trainee.details;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableMongock
 @SpringBootApplication
 public class TisTraineeDetailsApplication {
 
