c4e95a8349750cdbfa319f7679e65e200e75b120
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2022-10-21 15:53:02 +0100

    feat: par endpoint wip
    TODO: use actual placement details in credential token hint, and sort
    out a few other details.
    
    TIS21-3685

--- src/main/java/uk/nhs/hee/trainee/details/api/PlacementCredential.java
+++ src/main/java/uk/nhs/hee/trainee/details/api/PlacementCredential.java
@@ -21,31 +21,66 @@
 
 package uk.nhs.hee.trainee.details.api;
 
+import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import uk.nhs.hee.trainee.details.dto.PlacementDto;
 import uk.nhs.hee.trainee.details.mapper.PlacementMapper;
+import uk.nhs.hee.trainee.details.model.ParResponse;
 import uk.nhs.hee.trainee.details.model.Placement;
-import uk.nhs.hee.trainee.details.service.PlacementService;
+import uk.nhs.hee.trainee.details.service.JwtService;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/placementcredential")
 public class PlacementCredential {
 
-  private final PlacementService service;
+  private static final Integer CONNECT_TIMEOUT = 2000;
+  private static final Integer READ_TIMEOUT = 5000;
+
   private final PlacementMapper mapper;
 
-  public PlacementCredential(PlacementService service, PlacementMapper mapper) {
-    this.service = service;
+  private final String clientId;
+  private final String clientSecret;
+  private final String redirectUri;
+  private final String parEndpoint;
+
+  private final RestTemplateBuilder restTemplateBuilder;
+  private final JwtService jwtService;
+
+
+  public PlacementCredential(PlacementMapper mapper,
+                             @Value("${dsp.client-id}") String clientId,
+                             @Value("${dsp.client-secret}") String clientSecret,
+                             @Value("${dsp.redirect-uri}") String redirectUri,
+                             @Value("${dsp.par-endpoint}") String parEndpoint,
+                             RestTemplateBuilder restTemplateBuilder,
+                             JwtService jwtService) {
     this.mapper = mapper;
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.redirectUri = redirectUri;
+    this.parEndpoint = parEndpoint;
+    this.restTemplateBuilder = restTemplateBuilder;
+    this.jwtService = jwtService;
   }
 
   /**
@@ -57,25 +92,49 @@ public class PlacementCredential {
    * @return The Request URI, or error code.
    */
   @GetMapping("/par/{placementTisId}")
-  public String getCredentialParUri(
+  public ParResponse getCredentialParUri(
       @PathVariable(name = "placementTisId") String placementTisId,
       @RequestBody @Validated PlacementDto dto) {
     log.info("Get placement credential request URI for placement with TIS ID {}", placementTisId);
+
     Placement entity = mapper.toEntity(dto);
 
-    String client_id = "TODO";
-    String client_secret = "TODO";
-    String redirect_uri = "/credentialissued/" + placementTisId;
-    String scope = "issue.Placement"; //TODO confirm
-    String nonce = "";
-    String state = "";
-    String id_token_hint = entity.toCredentialJwt();
-    JSONObject parResponse = POST to OIDC_PAR(client_id, client_secret, redirect_uri, scope, nonce, state, id_token_hint);
-    ParResponse response = decode parResponse;
+    RestTemplate restTemplate = this.restTemplateBuilder
+        .setConnectTimeout(Duration.ofMillis(CONNECT_TIMEOUT))
+        .setReadTimeout(Duration.ofMillis(READ_TIMEOUT))
+        .build();
 
-    return response.requestUri;
+    URI parUri = URI.create(this.parEndpoint);
+
+    String scope = "issue.TestCredential"; //TODO set up issue.Placement
+    String nonce = UUID.randomUUID().toString();
+    String state = ""; //TODO
+    String idTokenHint = jwtService.generatePlacementToken(entity);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+    MultiValueMap<String, String> bodyPair = new LinkedMultiValueMap<>();
+    bodyPair.add("client_id", this.clientId);
+    bodyPair.add("client_secret", this.clientSecret);
+    bodyPair.add("redirect_uri", this.redirectUri);
+    bodyPair.add("scope", scope);
+    bodyPair.add("id_token_hint", idTokenHint);
+    bodyPair.add("nonce", nonce);
+    bodyPair.add("state", state);
+
+    HttpEntity<MultiValueMap<String, String>> parRequest = new HttpEntity<>(bodyPair, headers);
+
+    ResponseEntity<ParResponse> parResponse = restTemplate.postForEntity(parUri, parRequest, ParResponse.class);
+    if(parResponse.getStatusCode() == HttpStatus.CREATED && parResponse.getBody() != null) {
+      return parResponse.getBody();
+    } else {
+      return null;
+    }
+
     //Now Front-end / Sitekit makes Authorize request with requestUri - QR code shown to user for approval, etc.
-    //the authorize response then redirects to /credentialissued/123?code=xxx&state=yyy
+    //the authorize response then redirects to /profile/123?code=xxx&state=yyy
     //This page would need to call the API below to get the actual details of the token
   }
 
@@ -87,25 +146,6 @@ public class PlacementCredential {
    * @param state        state
    * @return The token for the credential
    */
-  @GetMapping("/token/{placementTisId}")
-  public String getCredentialIdToken(
-      @PathVariable(name = "placementTisId") String placementTisId,
-      @RequestParam String code,
-      @RequestParam String state) {
-    log.info("Get token for issued credential for placement ID {}", placementTisId);
-
-    //check state has not changed
-
-    String grant_type = "authorization_code";
-    String client_id = "TODO";
-    String client_secret = "TODO";
-    String redirect_uri = "/credentialissued/" + placementTisId; //must match one in PAR request
-    JSONObject id_token = POST ISSUING_TOKEN(grant_type, client_id, client_secret, redirect_uri, code);
-    //I don't know if the redirect_uri is actually used to redirect the browser, or if it's just a
-    // check field. I'm presuming the actual call to /issuing/authorize needs to happen from the
-    // back-end, since it contains the client_secret.
-
-    return id_token.toString();
-  }
+//TODO
 
 }
--- src/main/java/uk/nhs/hee/trainee/details/api/PlacementResourceJwt.java
+++
@@ -1,2 +0,0 @@
-package uk.nhs.hee.trainee.details.api;public class PlacementResourceJwt {
-}
