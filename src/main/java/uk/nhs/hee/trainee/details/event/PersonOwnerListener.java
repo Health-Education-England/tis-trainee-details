97fb757f97b363fd74f90e18c76fd605662d7653
Andy Dingley <Judge40@users.noreply.github.com>
2023-07-27 16:36:28 +0100

    feat: add event handlers for PersonDetails changes
    Create SQS listeners for the following events:
     - Basic detail updates
     - Contact details updates
     - GDC details updates
     - GMC details updates
     - Personal info updates
     - Person Owner updates
    
    TIS21-4076
    TIS21-4086

---
+++ src/main/java/uk/nhs/hee/trainee/details/event/PersonOwnerListener.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2023 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.trainee.details.event;
+
+import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
+import io.awspring.cloud.messaging.listener.annotation.SqsListener;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import uk.nhs.hee.trainee.details.dto.PersonalDetailsDto;
+import uk.nhs.hee.trainee.details.dto.PersonalDetailsEvent;
+import uk.nhs.hee.trainee.details.mapper.PersonalDetailsMapper;
+import uk.nhs.hee.trainee.details.model.PersonalDetails;
+import uk.nhs.hee.trainee.details.service.PersonalDetailsService;
+
+@Slf4j
+@Component
+public class PersonOwnerListener {
+
+  private final PersonalDetailsService service;
+  private final PersonalDetailsMapper mapper;
+
+  public PersonOwnerListener(PersonalDetailsService service, PersonalDetailsMapper mapper) {
+    this.service = service;
+    this.mapper = mapper;
+  }
+
+  /**
+   * Update the person owner for the trainee.
+   *
+   * @param event The sync event containing the person owner data.
+   */
+  @SqsListener(value = "${application.aws.sqs.person-owner-update", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+  void updatePersonOwner(PersonalDetailsEvent event) {
+    String tisId = event.metadata().tisId();
+    log.info("Update person owner of trainee with TIS ID {}", tisId);
+
+    PersonalDetailsDto dto = event.data();
+    PersonalDetails entity = mapper.toEntity(dto);
+    Optional<PersonalDetails> optionalEntity = service.updatePersonOwnerByTisId(tisId, entity);
+    optionalEntity.orElseThrow(() -> new IllegalArgumentException("Trainee not found."));
+  }
+}
