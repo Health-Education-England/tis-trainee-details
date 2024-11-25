1bda2675db92cd484af970e4a08c595e9bf8481e
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2024-11-25 08:50:34 +0000

    fix: unneeded import

--- src/test/java/uk/nhs/hee/trainee/details/api/TraineeProfileResourceTest.java
+++ src/test/java/uk/nhs/hee/trainee/details/api/TraineeProfileResourceTest.java
@@ -23,7 +23,6 @@ package uk.nhs.hee.trainee.details.api;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
