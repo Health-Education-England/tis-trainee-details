242f731d814c372389c25c40da8c6730d9b4bf16
Andy Dingley <Judge40@users.noreply.github.com>
2025-01-22 01:51:00 +0000

    build: migrate to version catalog
    Use the trainee version catalog to determine plugin and dependency
    versions.
    The version catalog was based on `tis-trainee-details` so there are
    currently no version changes to any dependendencies.
    
    NO-TICKET

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "tis-trainee-details"
+
+dependencyResolutionManagement {
+  repositories {
+    mavenCentral()
+  }
+
+  versionCatalogs {
+    create("libs") {
+      from("uk.nhs.tis.trainee:version-catalog:0.0.2")
+    }
+  }
+}
