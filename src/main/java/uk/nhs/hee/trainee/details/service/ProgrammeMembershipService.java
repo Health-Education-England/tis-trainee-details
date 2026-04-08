3538dfb292e1484fd0e639997846d35640948fbf
Reuben Roberts <reuben.roberts@nhs.net>
2026-04-08 15:27:03 +0100

    fix: remove overloaded function

--- src/main/java/uk/nhs/hee/trainee/details/service/ProgrammeMembershipService.java
+++ src/main/java/uk/nhs/hee/trainee/details/service/ProgrammeMembershipService.java
@@ -816,24 +816,6 @@ public class ProgrammeMembershipService {
   protected String getOwnerContact(List<Map<String, String>> ownerContactList,
       LocalOfficeContactType contactType, LocalOfficeContactType fallbackContactType,
       String defaultMessage) {
-    return getOwnerContact(ownerContactList, contactType, fallbackContactType, defaultMessage,
-        null);
-  }
-
-  /**
-   * Get specified owner contact from a list of contacts.
-   *
-   * @param ownerContactList    The owner contact list to search.
-   * @param contactType         The contact type to return.
-   * @param fallbackContactType if the contactType is not available, return this contactType
-   *                            instead.
-   * @param defaultMessage      The default message if the contact was not found.
-   * @param traineeType         The trainee type to use to qualify the contact type name.
-   * @return The specific contact of the owner, or the default message if not found.
-   */
-  protected String getOwnerContact(List<Map<String, String>> ownerContactList,
-      LocalOfficeContactType contactType, LocalOfficeContactType fallbackContactType,
-      String defaultMessage, TraineeType traineeType) {
 
     Optional<Map<String, String>> ownerContact = ownerContactList.stream()
         .filter(c ->
@@ -867,7 +849,7 @@ public class ProgrammeMembershipService {
       LocalOfficeContactType fallbackContactType, String defaultMessage, TraineeType traineeType) {
 
     return getOwnerContact(getOwnerContactList(localOfficeName, traineeType), contactType,
-        fallbackContactType, defaultMessage, traineeType);
+        fallbackContactType, defaultMessage);
   }
 
   /**
